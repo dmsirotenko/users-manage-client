d2dc9989029272791599b9327b8c13df5c54612e
Дмитрий <sirotenkod@zeptolab.com>
2019-07-14 22:28:12 +0400

    Some configuration + fontawesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
+
+import './assets/stylesheets/main.scss';
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({ render: h => h(App) }).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,6 @@ module.exports = {
         changeOrigin: true
       }
     }
-  }
+  },
+  assetsDir: 'static'
 };
